eef23ce8ca91ef3caaa089ebae0ad006b044b6e4
rafaelnutti <nuttirafael@gmail.com>
2022-03-23 12:25:49 -0300

    v1.0.1 Variable name change

--- Power.java
+++ Power.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 			Scanner ler = new Scanner(System.in);
 			float num1;
 			int num2;
-			System.out.print("Number: ");
+			System.out.print("Base: ");
 			num1 = ler.nextFloat();
 			System.out.print("Power: ");
 			num2 = ler.nextInt();
