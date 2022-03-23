54a187cfda660bd9f3b4305b7812289b5396bb1a
rafaelnutti <nuttirafael@gmail.com>
2022-03-23 12:21:57 -0300

    v1.0 Math operator power on Java

---
+++ Pow.java
@@ -0,0 +1,46 @@
+public class Pow {
+  public static void pow(float x, int y) {
+    double num = 1;
+    boolean det = true;
+    if (y > 0) {
+      for (int i = 1; i <= y; i++) {
+        num = num * x;
+      }
+    }
+    else if (y == 0) {
+      if (x == 0) {
+        det = false;
+      }
+      num = 1;
+    }
+    else {
+      for (int i = -1; i >= y; i--) {
+        num = num * x;
+      }
+      num = 1/num;
+    }
+    System.out.println();
+    if (det) {
+      if (y > 0) {
+        if (y % 10 == 1) {
+          System.out.println(x + " to the " + y + "st power equals to " + num);
+        }
+        else if (y % 10 == 2) {
+          System.out.println(x + " to the " + y + "nd power equals to " + num);
+        }
+        else if (y % 10 == 3) {
+          System.out.println(x + " to the " + y + "rd power equals to " + num);
+        }
+        else {
+          System.out.println(x + " to the " + y + "th power equals to " + num);
+        }
+      }
+      else {
+        System.out.println(x + " power " + y + " equals to " + num);
+      }
+    }
+    else {
+      System.out.println("No agreed-upon value to zero to the power of zero!");
+    }
+  }
+}
---
+++ Power.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+	class Power {
+		public static void main(String[] args) {
+			Scanner ler = new Scanner(System.in);
+			float num1;
+			int num2;
+			System.out.print("Number: ");
+			num1 = ler.nextFloat();
+			System.out.print("Power: ");
+			num2 = ler.nextInt();
+			new Pow().pow(num1,num2);
+		}
+	}
