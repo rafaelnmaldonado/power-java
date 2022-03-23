f72dc350dc2e06c7b71bf3ef13f7899bcc5e8097
rafaelnutti <nuttirafael@gmail.com>
2022-03-23 12:33:08 -0300

    v1.0.2 Mathematical terms change

--- Pow.java
+++ Pow.java
@@ -23,20 +23,20 @@ public class Pow {
     if (det) {
       if (y > 0) {
         if (y % 10 == 1) {
-          System.out.println(x + " to the " + y + "st power equals to " + num);
+          System.out.println(x + " raised to the " + y + "st power equals to " + num);
         }
         else if (y % 10 == 2) {
-          System.out.println(x + " to the " + y + "nd power equals to " + num);
+          System.out.println(x + " raised to the " + y + "nd power equals to " + num);
         }
         else if (y % 10 == 3) {
-          System.out.println(x + " to the " + y + "rd power equals to " + num);
+          System.out.println(x + " raised to the " + y + "rd power equals to " + num);
         }
         else {
-          System.out.println(x + " to the " + y + "th power equals to " + num);
+          System.out.println(x + " raised to the " + y + "th power equals to " + num);
         }
       }
       else {
-        System.out.println(x + " power " + y + " equals to " + num);
+        System.out.println(x + " raised the power of " + y + " equals to " + num);
       }
     }
     else {
